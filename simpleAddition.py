4a405be04e766f12a518ac6b48ee2b143928bfc1
MASKY-33 <masky.somebody33@gmail.com>
2026-06-06 01:11:32 +0200

    Create simpleAddition.py

---
+++ simpleAddition.py
@@ -0,0 +1,8 @@
+personFirst_number = int(input("Enter first number: "))
+personNext_number = int(input("Enter next number: "))
+
+result = personFirst_number + personNext_number  #A way to sum-up given numbers
+
+# result = (f"{personFirst_number + personNext_number}") #Other way (format-string) to sum-up given numbers
+
+print(personFirst_number + personNext_number)
